b372f7e7ea7aee3c2a7dd86114255c1f65e92106
Vishal-g9822 <vishalgaikwadgithub@gmail.com>
2024-06-11 23:25:52 +0530

    movielistpagedone

--- MainProj/FrontEnd1/moviebooking1/src/pages/MovieList.jsx
+++ MainProj/FrontEnd1/moviebooking1/src/pages/MovieList.jsx
@@ -1,7 +1,15 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import { Link } from "react-router-dom"
-import movieImage from "../moviePng/image.png"
+import aquaman from "./moviePng/aquaman.jpg"
+import avengers from "./moviePng/avengers.jpg"
+import conjuring from "./moviePng/conjuring.jpg"
+import image from "./moviePng/image.png"
+import justiceLeague from "./moviePng/justiceLeague.jpg"
+import missionImpossible from "./moviePng/missionImpossible.jpg"
+import shawshankredeemption from "./moviePng/shawshankredeemption.jpg"
+import spiderman from "./moviePng/spiderman.jpg"
+import titanic from "./moviePng/titanic.jpg"
 function MovieList(){
     return(
         <div>
@@ -13,51 +21,51 @@ function MovieList(){
                 
                 <div className="col">
                 <Link to="/movieDetails"> 
-                <div className="card myCard" style={{margin:30}}>
+                <div className="card myCard" >
                     <img style={{width:150}}
-                    className="card-img-top"src={movieImage} alt=""/>
+                    className="card-img-top"src={aquaman} alt=""/>
                     <div className="card-body">
-                        <div style={{fontWeight:'bold',fontSize:19}}> Matrix</div>
-                        <div>Price:<span style={{fontWeight:'bold',fontSize:17}}> Rs.550</span></div>
+                        <div className="card-body-title" > Aquaman</div>
+                        <div><span style={{fontWeight:'bold',fontSize:12}}> Rs.550</span></div>
                     </div>
                 </div>
                 </Link>
                 </div>
                 <div className="col">
                 <Link to="/movieDetails"> 
-                <div className="card myCard" style={{margin:30}}>
+                <div className="card myCard" >
                     <img style={{width:150}}
-                    className="card-img-top
-                    "src={movieImage} alt=""/>
+                    className="card-img-top"
+                     src={avengers} alt=""/>
                     <div className="card-body">
-                        <div style={{fontWeight:'bold',fontSize:19}}> Matrix</div>
-                        <div>Price:<span style={{fontWeight:'bold',fontSize:17}}> Rs.470</span></div>
+                        <div className="card-body-title"> Avengers</div>
+                        <div><span style={{fontWeight:'bold',fontSize:12}}> Rs.470</span></div>
                     </div>
                 </div>
                 </Link>
                 </div>
                 <div className="col">
                 <Link to="/movieDetails"> 
-                <div className="card myCard" style={{margin:30}}>
+                <div className="card myCard" >
                     <img style={{width:150}}
                     className="card-img-top
-                    "src={movieImage} alt=""/>
+                    "src={image} alt=""/>
                     <div className="card-body">
-                        <div style={{fontWeight:'bold',fontSize:19}}> Matrix</div>
-                        <div>Price:<span style={{fontWeight:'bold',fontSize:17}}> Rs.500</span></div>
+                        <div className="card-body-title"> Matrix</div>
+                        <div><span style={{fontWeight:'bold',fontSize:12}}> Rs.500</span></div>
                     </div>
                 </div>
                 </Link>
                 </div>
                 <div className="col">
                 <Link to="/movieDetails"> 
-                <div className="card myCard" style={{margin:30}}>
+                <div className="card myCard" >
                     <img style={{width:150}}
                     className="card-img-top
-                    "src={movieImage} alt=""/>
+                    "src={conjuring} alt=""/>
                     <div className="card-body">
-                        <div style={{fontWeight:'bold',fontSize:19}}> Matrix</div>
-                        <div>Price:<span style={{fontWeight:'bold',fontSize:17}}> Rs.300</span></div>
+                        <div className="card-body-title"> Conjuring</div>
+                        <div><span style={{fontWeight:'bold',fontSize:12}}> Rs.300</span></div>
                     </div>
                 </div>
                 </Link>
@@ -69,52 +77,52 @@ function MovieList(){
                 
                 <div className="col">
                 <Link to="/movieDetails"> 
-                <div className="card myCard" style={{margin:30}}>
+                <div className="card myCard" >
                     <img style={{width:150}}
                     className="card-img-top
-                    "src={movieImage} alt=""/>
+                    "src={justiceLeague} alt=""/>
                     <div className="card-body">
-                        <div style={{fontWeight:'bold',fontSize:19}}> Matrix</div>
-                        <div>Price:<span style={{fontWeight:'bold',fontSize:17}}> Rs.650</span></div>
+                        <div className="card-body-title"> Justice League</div>
+                        <div><span style={{fontWeight:'bold',fontSize:12}}> Rs.650</span></div>
                     </div>
                 </div>
                 </Link>
                 </div>
                 <div className="col">
                 <Link to="/movieDetails"> 
-                <div className="card myCard" style={{margin:30}}>
+                <div className="card myCard" >
                     <img style={{width:150}}
                     className="card-img-top
-                    "src={movieImage} alt=""/>
+                    "src={missionImpossible} alt=""/>
                     <div className="card-body">
-                        <div style={{fontWeight:'bold',fontSize:19}}> Matrix</div>
-                        <div>Price:<span style={{fontWeight:'bold',fontSize:17}}> Rs.450</span></div>
+                        <div className="card-body-title"> Mission Impossible</div>
+                        <div><span style={{fontWeight:'bold',fontSize:12}}> Rs.450</span></div>
                     </div>
                 </div>
                 </Link>
                 </div>
                 <div className="col">
                 <Link to="/movieDetails"> 
-                <div className="card myCard" style={{margin:30}}>
+                <div className="card myCard" >
                     <img style={{width:150}}
                     className="card-img-top
-                    "src={movieImage} alt=""/>
+                    "src={shawshankredeemption} alt=""/>
                     <div className="card-body">
-                        <div style={{fontWeight:'bold',fontSize:19}}> Matrix</div>
-                        <div>Price:<span style={{fontWeight:'bold',fontSize:17}}> Rs.370</span></div>
+                        <div className="card-body-title"> Shawshank Redeemption</div>
+                        <div><span style={{fontWeight:'bold',fontSize:12}}> Rs.370</span></div>
                     </div>
                 </div>
                 </Link>
                 </div>
                 <div className="col">
                 <Link to="/movieDetails"> 
-                <div className="card myCard" style={{margin:30}}>
+                <div className="card myCard" >
                     <img style={{width:150}}
                     className="card-img-top
-                    "src={movieImage} alt=""/>
+                    "src={spiderman} alt=""/>
                     <div className="card-body">
-                        <div style={{fontWeight:'bold',fontSize:19}}> Matrix</div>
-                        <div>Price:<span style={{fontWeight:'bold',fontSize:17}}> Rs.350</span></div>
+                        <div className="card-body-title"> Spiderman</div>
+                        <div><span style={{fontWeight:'bold',fontSize:12}}> Rs.350</span></div>
                     </div>
                 </div>
                 </Link>
