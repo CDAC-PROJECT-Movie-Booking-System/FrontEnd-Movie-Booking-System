13da909cbeb9d8ec64e3d6f85d94d4e01ebc4a1b
adisat456 <adisat456@gmail.com>
2024-06-17 23:19:09 +0530

    Movie Item Cards

--- MainProj/FrontEnd1/moviebooking1/src/components/movielist/MovieList.jsx
+++ MainProj/FrontEnd1/moviebooking1/src/components/movielist/MovieList.jsx
@@ -11,110 +11,44 @@ import shawshankredeemption from "../../pages/moviePng/shawshankredeemption.jpg"
 import spiderman from "../../pages/moviePng/spiderman.jpg"
 // import titanic from "./moviePng/titanic.jpg"
 import './MovieList.css';
+
 function MovieList(){
     return(
         <div>
-    <Navbar/>
-    <h2 className="page-header">MovieList</h2>
-    <div className="container">
-        <div className="row">
-            <div className="col-md-3 col-sm-6">
-                <Link to="/movieDetails"> 
-                    <div className="card myCard">
-                        <img className="card-img-top" src={aquaman} alt="Aquaman"/>
-                        <div className="card-body">
-                            <div className="card-body-title">Aquaman</div>
-                            <div><span>Rs.550</span></div>
-                        </div>
-                    </div>
-                </Link>
+        <Navbar />
+        <h2 className="page-header">MovieList</h2>
+        <div className="container">
+            <div className="row">
+                <MovieCard image={aquaman} title="Aquaman" languages={['English', 'Spanish']} />
+                <MovieCard image={avengers} title="Avengers" languages={['English', 'French']} />
+                <MovieCard image={image} title="Matrix" languages={['English', 'German']} />
+                <MovieCard image={conjuring} title="Conjuring" languages={['English']} />
             </div>
-            <div className="col-md-3 col-sm-6">
-                <Link to="/movieDetails"> 
-                    <div className="card myCard">
-                        <img className="card-img-top" src={avengers} alt="Avengers"/>
-                        <div className="card-body">
-                            <div className="card-body-title">Avengers</div>
-                            <div><span>Rs.470</span></div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="col-md-3 col-sm-6">
-                <Link to="/movieDetails"> 
-                    <div className="card myCard">
-                        <img className="card-img-top" src={image} alt="Matrix"/>
-                        <div className="card-body">
-                            <div className="card-body-title">Matrix</div>
-                            <div><span>Rs.500</span></div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="col-md-3 col-sm-6">
-                <Link to="/movieDetails"> 
-                    <div className="card myCard">
-                        <img className="card-img-top" src={conjuring} alt="Conjuring"/>
-                        <div className="card-body">
-                            <div className="card-body-title">Conjuring</div>
-                            <div><span>Rs.300</span></div>
-                        </div>
-                    </div>
-                </Link>
+            <div className="row">
+                <MovieCard image={justiceLeague} title="Justice League" languages={['English', 'Italian']} />
+                <MovieCard image={missionImpossible} title="Mission Impossible" languages={['English', 'Japanese']} />
+                <MovieCard image={shawshankredeemption} title="Shawshank Redemption" languages={['English', 'Chinese']} />
+                <MovieCard image={spiderman} title="Spiderman" languages={['English', 'Korean']} />
             </div>
         </div>
-        <div className="row">
-            <div className="col-md-3 col-sm-6">
-                <Link to="/movieDetails"> 
-                    <div className="card myCard">
-                        <img className="card-img-top" src={justiceLeague} alt="Justice League"/>
-                        <div className="card-body">
-                            <div className="card-body-title">Justice League</div>
-                            <div><span>Rs.650</span></div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="col-md-3 col-sm-6">
-                <Link to="/movieDetails"> 
-                    <div className="card myCard">
-                        <img className="card-img-top" src={missionImpossible} alt="Mission Impossible"/>
-                        <div className="card-body">
-                            <div className="card-body-title">Mission Impossible</div>
-                            <div><span>Rs.450</span></div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="col-md-3 col-sm-6">
-                <Link to="/movieDetails"> 
-                    <div className="card myCard">
-                        <img className="card-img-top" src={shawshankredeemption} alt="Shawshank Redemption"/>
-                        <div className="card-body">
-                            <div className="card-body-title">Shawshank Redemption</div>
-                            <div><span>Rs.370</span></div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="col-md-3 col-sm-6">
-                <Link to="/movieDetails"> 
-                    <div className="card myCard">
-                        <img className="card-img-top" src={spiderman} alt="Spiderman"/>
-                        <div className="card-body">
-                            <div className="card-body-title">Spiderman</div>
-                            <div><span>Rs.350</span></div>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-        </div>
+        <Footer />
     </div>
-    <Footer/>
-</div>
-
-
     )
 }
+const MovieCard = ({ image, title, languages }) => {
+    return (
+        <div className="col-md-3 mb-4 col-sm-6">
+            <Link to="/movieDetails">
+                <div className="card myCard">
+                    <img className="card-img-top" src={image} alt={title} />
+                    <div className="card-body">
+                    </div>
+                </div>
+                        <div className="card-body-text card-body-title">{title}</div>
+                        <div className="card-body-text card-body-lan">{languages.join(', ')}</div>
+            </Link>
+        </div>
+    );
+};
 
 export default MovieList
