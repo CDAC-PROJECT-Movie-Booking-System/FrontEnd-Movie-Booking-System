7b0edbedd68321c82bf8b6087c2784a4deaee2e3
adisat456 <adisat456@gmail.com>
2024-08-09 16:20:28 +0530

    Pagination

--- MainProj/FrontEnd1/moviebooking1/src/Services/MovieList.js
+++ MainProj/FrontEnd1/moviebooking1/src/Services/MovieList.js
@@ -2,18 +2,15 @@ import axios from "axios";
 import config from "../config";
 import { toast } from "react-toastify";
 
-// export async function fetchMovieDetails(email, password){
-//     const body = {
-//         email, password
-//     }
-//     try{
-//         const response = await axios.post(`${config.url}/users/signin`,body);
-//         return response.data;
-//     }catch(error){
-//         console.log('error-response', error);
-//         toast.error(error.response.data.message)
-//     }
-// }
+export async function fetchMovieDetails(pageNumber){     
+    try{
+        const response = await axios.get(`${config.url}/moviestest/pagenumber/${pageNumber}`);
+        return response.data;
+    }catch(error){
+        console.log('error-response', error);
+        // toast.error(error.response.data.message)
+    }
+}
 
 // export async function fetchSliderDetails(firstName, lastName, email, password){
 //     const body = {
